369b33a8a8a946150035f793fa50a0418605570c
Alec Z <az94587@gmail.com>
2021-10-07 00:24:07 -0700

    cam zoom ?

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Class to control Camera Movement
+// currently only used to control camera zoom on selected objects
+public class CameraManager : MonoBehaviour
+{
+
+    private Camera cam;
+    private Vector3 init_camPos;
+    public Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = this.GetComponent<Camera>();
+        init_camPos = this.transform.position;
+        target = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * 2);
+        
+        //if (this.transform.position == target)
+        //    target = null;
+    }
+
+    // zooms in the camera on a specific gameObject position
+    public void cameraZoom(Vector3 toZoomPosition)
+    {
+        // find vector 3 for distance to travel
+        //Vector3 dist = new Vector3(toZoomPosition.x, toZoomPosition.x, 0) ;
+
+        if(target != null)
+            Debug.Log("Changing Targets while target is already set");
+
+
+        // only go half-way to z so that we can still see the object
+        target = new Vector3(toZoomPosition.x, toZoomPosition.y, init_camPos.z / 2f);
+    }
+
+    // Sets target to initial camera position
+    public void cameraReset()
+    {
+        target = init_camPos;
+    }
+}
--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    public bool interacted = false;
-    [SerializeField] private Sprite interactedSprite;
+    public bool interacted = false;     // true if object has been interacted with
+    public bool selected = false;       // true if object is currently being interacted with (cam zoom?)
+    
+    private Sprite defaultSprite;       // the original sprite to be rendered
+
+    // can possibly turn this into an array if more sprite "stages" are desired
+    [SerializeField] private Sprite interactedSprite;   // new sprite to render after being interacted with
+
+    private CameraManager mainCam;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        defaultSprite = this.GetComponent<SpriteRenderer>().sprite;
+        mainCam = GameObject.FindWithTag("MainCamera").GetComponent<CameraManager>();
     }
 
     // Update is called once per frame
@@ -27,22 +35,25 @@ public class Interactable : MonoBehaviour
 
         //this.interacted = !this.interacted;     //swap interacted value
         this.interacted = true;
+        this.selected = true;
+        mainCam.cameraZoom(this.transform.position);
         
         Debug.Log("Clicked on " + this.name);
 
         // OPTIONAL: change the object sprite if a new one is assigned
-        // can make interactedSprite an array of sprites if multiple "phases" are desired
         if(this.interactedSprite)
             this.GetComponent<SpriteRenderer>().sprite = interactedSprite;
         
     }
 
-    // RIGHT CLICK ON AN OBJECT - debug.log the status of this object
+    // RIGHT CLICK ON AN OBJECT - stop focus on object (reset camera)
     // detecting right click on an object using a different approach
     void OnMouseOver () {
         if (Input.GetMouseButtonDown(1)) 
         {
-            Debug.Log(this.name + " is " + (interacted ? "selected" : "not selected") );
+            //Debug.Log(this.name + " is " + (interacted ? "selected" : "not selected") );
+            this.selected = false;
+            mainCam.cameraReset();
         }
     }
     
